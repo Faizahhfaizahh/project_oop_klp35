5e9f076a193de9e31719693508e03f6eb1f81618
Faizahhh03 <faizahf665@gmail.com>
2024-12-07 22:27:48 +0800

    progres 3

---
+++ UI.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class UI implements Runnable {
+    private JFrame frame;
+    private GUISlidingNumber puzzleAngka;
+    private int dimension;
+    private int margin;
+
+    public UI(int dimension, int margin) {
+        this.dimension = dimension;
+        this.margin = margin;
+    }
+
+    @Override
+    public void run() {
+        // Inisialisasi frame
+        frame = createFrame();
+        
+        // Tambahkan komponen ke frame
+        setupFrameComponents();
+        
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    private JFrame createFrame() {
+        JFrame frame = new JFrame("Sliding Puzzle Game");
+        frame.setPreferredSize(new Dimension(dimension + 200, dimension + margin));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setResizable(true);
+        return frame;
+    }
+
+    private void setupFrameComponents() {
+        puzzleAngka = new GUISlidingNumber(dimension, margin);
+
+        Container contentPane = frame.getContentPane();
+        contentPane.setLayout(new BorderLayout());
+
+        contentPane.add(puzzleAngka, BorderLayout.CENTER);
+    }
+
+    public JFrame getFrame() {
+        return frame;
+    }
+}
