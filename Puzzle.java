af6f30a6459245f007dca9471cf82b766fa300c6
Faizahhh03 <faizahf665@gmail.com>
2024-12-07 22:24:52 +0800

    progres 3

--- Puzzle.java
+++ Puzzle.java
@@ -3,10 +3,11 @@ import javax.swing.SwingUtilities;
 public class Puzzle {
 
   public void startGame() {
+    UI uiSliding = new UI(600, 30); 
+    SwingUtilities.invokeLater(uiSliding);
     
 }
   public static void main(String[] args) {
-    
     SwingUtilities.invokeLater(() -> new Login().setVisible(true));
   }
 }
